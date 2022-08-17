8bf6331191506848d63e76f2ba5391d798dc7d9d
Jafta <201435755@student.uj.ac.za>
2022-08-17 10:59:58 +0200

    add my files

---
+++ pizza-cart-api.js
@@ -0,0 +1,101 @@
+document.addEventListener('alpine:init', () => {
+    
+    Alpine.data('pizzaCartWithAPIWidget', function () {
+        return {
+            init() {
+                
+                axios
+                    .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+                    .then((result) => {
+                        this.pizzas = result.data.pizzas
+                    })
+                    .then(() => {
+                        return this.createCart();
+                    })
+                    .then((result) => {
+                        
+                        this.cartId = result.data.cart_code;
+                    });
+            },
+            createCart() {
+            
+                return axios.get('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username=' + this.username)
+            },
+            showCart() {
+                const url = `https://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get`;
+                axios
+                    .get(url)
+                    .then((result) => {
+                        this.cart = result.data;
+                    });
+            },
+            
+           
+            message: '',
+            username: 'JAY JAY',
+            pizzas: [],
+            cartId: '',
+            cart: { total: 0 },
+            payNow: false,
+            paymentAmount: 0,
+            paymentMessage: '',
+
+            add(pizza) {
+               
+                const params = {
+                    cart_code: this.cartId,
+                    pizza_id: pizza.id
+                }
+                axios
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/add', params)
+                    .then(() => {
+                        this.message = alert("Pizza added to the cart!")
+                        this.showCart();
+                    })
+                    .catch(err => alert(err));
+                
+            },
+
+            remove(pizza){
+                
+                const params = {
+                  cart_code : this.cartId,
+                  pizza_id : pizza.id
+                }
+                axios
+                  .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/remove', params)
+                  .then(()=>{
+                    this.message= alert("Pizza removed from the cart")
+                    this.showCart();
+                  })
+                  .catch(err=>alert(err));
+              },
+
+              pay(pizza){
+                const params = {
+                  cart_code : this.cartId,
+                }
+                axios
+                  .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/pay', params)
+                  .then(()=>{
+                      if(!this.paymentAmount){
+                          this.paymentMessage = 'No amount entered!'
+                      }
+                      else if(this.paymentAmount >= this.cart.total.toFixed(2)){
+                          this.paymentMessage = 'Payment Sucessful!'
+                          this.message= this.username  + " Paid!"
+                          setTimeout(() => {
+                              this.cart.total = ''
+                          }, 3000);
+                      }else{
+                          this.paymentMessage = 'Insufficient fund sorry!!!'
+                          setTimeout(() => {
+                              this.cart.total = ''
+                          }, 5000);
+                      }
+                  })
+                  .catch(err=>alert(err));
+              },
+        }
+    });
+})
